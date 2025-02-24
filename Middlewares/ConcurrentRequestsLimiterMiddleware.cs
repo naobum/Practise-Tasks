1563bbdc56e849e05f40b2199d0aeea20e20b6ed
naobum <artur130605@gmail.com>
2025-02-25 01:32:02 +0300

    Added parallel limit.

---
+++ Middlewares/ConcurrentRequestsLimiterMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Options;
+using Practise_Tasks.Settings;
+
+namespace Practise_Tasks.Middlewares
+{
+    public class ConcurrentRequestsLimiterMiddleware
+    {
+        private static int _currentRequests;
+        private readonly RequestDelegate _next;
+        private readonly int _parallelLimit;
+
+        public ConcurrentRequestsLimiterMiddleware(RequestDelegate next, IOptions<ServiceSettings> settings)
+        {
+            _next = next;
+            _parallelLimit = settings.Value.ParallelLimit;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (Interlocked.Increment(ref _currentRequests) > _parallelLimit)
+            {
+                Interlocked.Decrement(ref _currentRequests);
+                context.Response.StatusCode = StatusCodes.Status503ServiceUnavailable;
+                context.Response.ContentType = "text/plain";
+                await context.Response.WriteAsync("Сервер сейчас занят, попробуйте позже.");
+                return;
+            }
+
+            try
+            {
+                await _next(context);
+            }
+            finally
+            {
+                Interlocked.Decrement(ref _currentRequests);
+            }
+        }
+    }
+}
